aba0d550f316fdcc6284a3591301825c6fb741b7
Taisia Pitko <minydas@yandex.ru>
2021-03-31 23:54:32 +0300

    initial working

--- examples/src/__tests__/simple.test.ts
+++ examples/src/__tests__/simple.test.ts
@@ -1,7 +1,12 @@
-import { someUseful } from '../../../src';
+import { Test } from '../../../src';
 
 describe('simple-suite', () => {
-  it('simple-test', async () => {
-    someUseful();
-  });
+  Test('test')
+    .each([{ fruit: 'banana' }, { fruit: 'strawberry' }])
+    .each([{ addition: 'milk' }, { addition: 'ice-cream' }])
+    .run(async (t) => {
+      console.log(`${t.fruit} ${t.addition}`);
+      expect(t.fruit).toMatch(/\w*/);
+      expect(t.addition).toMatch(/\w*/);
+    });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,38 @@
-export const someUseful = () => {
-  console.log('Useful i hope');
-};
+// type Combined<T, K> = T & K;
+
+class TestInt<T = {}> {
+  eaches: T[] = [];
+  desc: string = '';
+
+  constructor(desc: string, input?: TestInt<T>) {
+    this.desc = desc;
+    if (input?.eaches) {
+      this.eaches = input?.eaches;
+    }
+  }
+
+  each<K>(cases: K[]): TestInt<T & K> {
+    if (this.eaches.length > 0) {
+      let newM: (T & K)[] = [];
+      this.eaches.forEach((x) => {
+        cases.forEach((k) => newM.push({ ...x, ...k }));
+      });
+      this.eaches = newM;
+      return this as any;
+    }
+    this.eaches.push(...(cases as (T & K)[]));
+    return this as any;
+  }
+
+  run(body: (each: T) => void) {
+    describe(this.desc, () => {
+      for (const each of this.eaches) {
+        it(`${JSON.stringify(each)}`, async () => {
+          await body(each);
+        });
+      }
+    });
+  }
+}
+
+export const Test = (desc: string) => new TestInt(desc);
