8bca346c233d0390c64d543ae55e940d49b41edc
Taisia Pitko <minydas@yandex.ru>
2021-04-03 14:07:10 +0300

    v0.3.3

--- src/index.ts
+++ src/index.ts
@@ -6,4 +6,11 @@ const createTest = (desc?: string) => new TestEach(desc);
 (global as any).Test = createTest;
 (global as any).its = createTest;
 
+type TestEachFunc = (desc?: string) => TestEach;
+
+declare global {
+  export const its: TestEachFunc;
+  export const Test: TestEachFunc;
+}
+
 export { TestEachSetup, TestEachEnv };
