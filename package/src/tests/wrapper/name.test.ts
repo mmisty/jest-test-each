9c01ecbd0268293579288fbded647081b835f8c1
Taisia Pitko <minydas@yandex.ru>
2021-04-04 18:19:45 +0300

    added tests for naming

---
+++ package/src/tests/wrapper/name.test.ts
@@ -0,0 +1,105 @@
+import { assertAll, success } from '../utils/utils';
+import { cleanup, createTest, result, waitFinished } from '../utils/runner-env';
+
+const rootName = 'Test pack - root';
+const test = () => createTest(rootName);
+const config = { groupBySuites: true, numericCases: false };
+
+describe('naming', () => {
+  beforeEach(() => {
+    cleanup();
+  });
+
+  const data = [
+    { desc: 'simple string', case: { simple: '1' }, expName: 'simple: 1' },
+    { desc: 'simple num', case: { simple: 1 }, expName: 'simple: 1' },
+    { desc: '2 props', case: { simple: 1, simple2: 'f' }, expName: 'simple: 1, simple2: f' },
+    { desc: 'simple array', case: { simple: [1, 2, 3] }, expName: 'simple: [1, 2, 3]' },
+    {
+      desc: 'complex array',
+      case: { simple: [{ el: 1 }, { el: 2 }, { el: 3 }] },
+      expName: 'simple: [el: 1, el: 2, el: 3]',
+    },
+    {
+      desc: 'more complex array',
+      case: { simple: [{ el: { inside: 5 } }, { el: { inside: 11 } }, { el: 3 }] },
+      expName: 'simple: [el: {inside: 5}, el: {inside: 11}, el: 3]',
+    },
+
+    {
+      desc: 'function inside',
+      case: { simple: [{ el: { inside: 5 } }, { el: { inside: () => {} } }, { el: 3 }] },
+      expFail: true,
+      error: `Too complex obj in test - please specify 'desc' for the case`,
+      expName: undefined,
+    },
+    {
+      desc: 'function inside with desc',
+      case: {
+        desc: 'test desc',
+        simple: [{ el: { inside: 5 } }, { el: { inside: () => {} } }, { el: 3 }],
+      },
+
+      expName: 'test desc',
+    },
+    {
+      desc: 'func simple',
+      case: { func: () => {} },
+      expFail: true,
+      error: `Too complex obj in test - please specify 'desc' for the case`,
+      expName: undefined,
+    },
+    {
+      desc: 'long name - err',
+      case: {
+        simple: [
+          { el: { inside: 5 } },
+          { el: { inside: 5 } },
+          { el: { inside: 5 } },
+          { el: { inside: 5 } },
+          { el: { inside: 11 } },
+          { el: 3 },
+        ],
+      },
+      expFail: true,
+      error: `Case name is too long (>100 symbols), please specify desc`,
+    },
+    {
+      desc: 'long name with desc',
+      case: {
+        desc: 'long name',
+        simple: [
+          { el: { inside: 5 } },
+          { el: { inside: 5 } },
+          { el: { inside: 5 } },
+          { el: { inside: 5 } },
+          { el: { inside: 11 } },
+          { el: 3 },
+        ],
+      },
+      expName: 'long name',
+    },
+  ];
+
+  data.forEach(p => {
+    it(p.desc, async () => {
+      let err1: Error | undefined = undefined;
+      try {
+        test()
+          .config(config)
+          .each([p.case as any])
+          .run(t => success());
+
+        await waitFinished();
+      } catch (err) {
+        err1 = err;
+      }
+
+      assertAll(
+        !p.expFail
+          ? () => expect(result.tests[0]).toBe(p.expName)
+          : () => expect(err1?.message).toBe(p.error),
+      );
+    });
+  });
+});
--- package/src/utils/name.ts
+++ package/src/utils/name.ts
@@ -1,22 +1,46 @@
-export const getName = <T>(obj: T) => {
+export const getName = <T>(obj: T): string => {
   const untypedObj = obj as any;
   const desc = untypedObj.desc;
   const flatDesc = untypedObj.flatDesc;
-  const combined = () =>
-    Object.getOwnPropertyNames(obj)
-      .filter(p => p !== 'desc')
-      .map(p => {
-        if (Array.isArray(untypedObj[p])) {
-          return `${p}: [${untypedObj[p]}]`;
-        }
+  const result =
+    flatDesc || (desc ? (typeof desc === 'function' ? desc(obj) : desc) : getNameInt(obj));
 
-        if (typeof untypedObj[p] === 'object') {
-          return `${p}: ${JSON.stringify(untypedObj[p])}`;
-        }
+  if (result.length > 100) {
+    throw new Error('Case name is too long (>100 symbols), please specify desc');
+  }
 
-        return `${p}: ${untypedObj[p]}`;
-      })
-      .join(', ');
+  return result;
+};
 
-  return flatDesc || (desc ? (typeof desc === 'function' ? desc(obj) : desc) : combined());
+const isSimple = (obj: any) => {
+  return typeof obj === 'string' || typeof obj === 'number';
+};
+
+const getNameInt = (obj: any): string => {
+  if (typeof obj === 'function') {
+    throw new Error(`Too complex obj in test - please specify 'desc' for the case`);
+  }
+  const joinSymbol = ', ';
+  return Object.getOwnPropertyNames(obj)
+    .filter(p => p !== 'desc')
+    .map(p => {
+      if (Array.isArray(obj[p])) {
+        return `${p}: [${obj[p]
+          .map((k: any) => (isSimple(k) ? k : getNameInt(k)))
+          .join(joinSymbol)}]`;
+      }
+
+      if (typeof obj[p] === 'function') {
+        throw new Error(`Too complex obj in test - please specify 'desc' for the case`);
+      }
+
+      if (typeof obj[p] === 'object') {
+        return `${p}: {${Object.getOwnPropertyNames(obj[p])
+          .map(k => (isSimple(obj[p][k]) ? `${k}: ${obj[p][k]}` : getNameInt(obj[p][k])))
+          .join(joinSymbol)}}`;
+      }
+
+      return `${p}: ${obj[p]}`;
+    })
+    .join(joinSymbol);
 };
